8eb5d9038c8c7e8a8349373a695edb52056065e6
Alanssrv <alanssrv@gmail.com>
2023-08-14 20:08:08 -0300

    adicionando novos videos

---
+++ js/adicionarVideo.js
@@ -0,0 +1,20 @@
+import { conectaApi } from "./conectaApi.js";
+
+const formInput = document.querySelector('[data-formulario]');
+
+formInput.addEventListener('submit', evento => criarVideo(evento));
+
+async function criarVideo(evento) {
+    evento.preventDefault();
+
+    const url = document.querySelector('[data-url]').value;
+    const titulo = document.querySelector('[data-titulo]').value;
+    const imagem = document.querySelector('[data-imagem]').value;
+
+    const descricao = Math.floor(Math.random() * 50 + 1).toString();
+    
+    await conectaApi.criarVideo(titulo, descricao, url, imagem);
+
+    window.location.href = '../pages/envio-concluido.html';
+}
+
--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,13 +1,32 @@
 const acessoAPI = 'http://localhost:3000/videos';
 
 async function listarVideos() {
-    const response = await fetch(acessoAPI);
-    const responseJson = await response.json();
+    const conexao = await fetch(acessoAPI);
+    const conexaoJsonResponse = await conexao.json();
     
-    return responseJson;
+    return conexaoJsonResponse;
+}
+
+async function criarVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch(acessoAPI, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoJsonResponse = await conexao.json();
+    return conexaoJsonResponse;
 }
 
 export const conectaApi = {
-    listarVideos
+    listarVideos,
+    criarVideo
 }
 
